b341bc973a248946f7c0c04273b9a7c5cba84c1d
RevTK <109392408+RevTK@users.noreply.github.com>
2023-04-15 18:07:55 +0900

    Bean register
    added annoatation

--- src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -4,6 +4,7 @@ import com.squarecross.photoalbum.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 
 }
