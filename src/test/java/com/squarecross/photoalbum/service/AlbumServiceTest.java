c9c5dcf68b6d371e170aad2e962d851809220370
RevTK <109392408+RevTK@users.noreply.github.com>
2023-04-15 18:17:28 +0900

    generate getAlbum test

---
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -0,0 +1,12 @@
+package com.squarecross.photoalbum.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlbumServiceTest {
+
+    @Test
+    void getAlbum() {
+    }
+}
