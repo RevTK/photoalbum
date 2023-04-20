a6dd82d27b998e2fd5c01c9a77fc398c11e92537
RevTK <109392408+RevTK@users.noreply.github.com>
2023-04-20 20:10:25 +0900

    #11 삭제

--- src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/AlbumRepository.java
@@ -9,8 +9,5 @@ import java.util.List;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
-    //앨범명으로 앨범 목록을 조회
-    List<Album> findByAlbumName(String name);
-    //Album 내 Photo 중 입력된 파일명을 가지고있는 앨범 리스트 추출
-    List<Photo> findByPhoto_FileName(String name);
+
 }
